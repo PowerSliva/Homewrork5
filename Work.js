752da8e585fadf1346958867a9e5e4e63251d342
PowerSliva <71924695+PowerSliva@users.noreply.github.com>
2020-11-07 19:16:44 +0700

    Update Work.js

--- Work.js
+++ Work.js
@@ -1,36 +1,33 @@
-let myass = [];
 class CustomArray{
+
   constructor(element){
-    this.num = element; 
-  }                 
+    this.num = element;
+      }     
+ 
   method1(){                     //Добавление элемента в начало массива
+    let myass = [this.num];
     myass.unshift(this.num);
     console.log(this.num);
     console.log(myass);
   }
   method2(){                      //Добавление элемента в конец массива
+    let myass = [this.num];
     myass.push(this.num);
     console.log(this.num);
     console.log(myass);
   }
   method3(){                      //Удаление последнего элемента
+    let myass = [this.num];
     myass.pop();
     console.log(myass);
   }
 }
 
-let numer1 = new CustomArray("Гав-Гав");
+let numer1 = new CustomArray(5);
 numer1.method1();
 
-let numer2 = new CustomArray("2 раза");
+let numer2 = new CustomArray(6);
 numer2.method2();
 
-let numer3 = new CustomArray("Мяу-мяу");
-numer3.method2();
-
-let numer4 = new CustomArray("256 раз");
-numer4.method2();
-
-let numer5 = new CustomArray("Здрастье, ой, извиняюсь не туда");
-numer5.method2();
-numer5.method3();
+let numer3 = new CustomArray(7);
+numer3.method3();
